527b3f8f79d0f49626c911a038dc3304a572f741
Ezra Luvaha <mrluvaha@gmail.com>
2024-05-13 00:10:02 +0300

    Third Commit

---
+++ client/src/scenes/global/Footer.jsx
@@ -0,0 +1,71 @@
+import { useTheme } from "@emotion/react";
+import { Box, Typography } from "@mui/material";
+import { shades } from "../../theme";
+
+function Footer() {
+  const {
+    palette: { neutral },
+  } = useTheme();
+  return (
+    <Box marginTop="70px" padding="40px 0" backgroundColor={neutral.light}>
+      <Box
+        width="80%"
+        margin="auto"
+        display="flex"
+        justifyContent="space-between"
+        flexWrap="wrap"
+        rowGap="30px"
+        columnGap="clamp(20px, 30px, 40px)"
+      >
+        <Box width="clamp(20%, 30%, 40%)">
+          <Typography
+            variant="h4"
+            fontWeight="bold"
+            mb="30px"
+            color={shades.secondary[500]}
+          >
+            Shelf
+          </Typography>
+          <div>
+            Step into the spotlight with Shelf, where fashion meets elegance. Shop quality, sustainability, and the latest trends—all in one place. Elevate your wardrobe today!
+          </div>
+        </Box>
+
+        <Box>
+          <Typography variant="h4" fontWeight="bold" mb="30px">
+            About Us
+          </Typography>
+          <Typography mb="30px">Careers</Typography>
+          <Typography mb="30px">Our Stores</Typography>
+          <Typography mb="30px">Terms & Conditions</Typography>
+          <Typography mb="30px">Privacy Policy</Typography>
+        </Box>
+
+        <Box>
+          <Typography variant="h4" fontWeight="bold" mb="30px">
+            Customer Care
+          </Typography>
+          <Typography mb="30px">Help Center</Typography>
+          <Typography mb="30px">Track Your Order</Typography>
+          <Typography mb="30px">Corporate & Bulk Purchasing</Typography>
+          <Typography mb="30px">Returns & Refunds</Typography>
+        </Box>
+
+        <Box width="clamp(20%, 25%, 30%)">
+          <Typography variant="h4" fontWeight="bold" mb="30px">
+            Contact Us
+          </Typography>
+          <Typography mb="30px">
+            Komarock, Phase 5
+          </Typography>
+          <Typography mb="30px" sx={{ wordWrap: "break-word" }}>
+            Email: shelfstore@gmail.com
+          </Typography>
+          <Typography mb="30px">(+254)111-444-333</Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default Footer;
--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,5 +1,13 @@
+import MainCarousel from "./MainCarousel";
+import ShoppingList from "./ShoppingList";
+import Subscribe from "./Subscribe";
+
 const Home = () => {
-    return <div>Home</div>;
+    return <div className='home'>
+        <MainCarousel />
+        <ShoppingList />
+        <Subscribe />
+    </div>;
 };
 
 export default Home;
